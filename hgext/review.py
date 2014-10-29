e5163108ecd695fd5be9ddc340a641b2458544a7
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2014-10-29 18:59:52 +0100

    fold lglb-tools@b1a86a736dec into logilab.devtools (closes #269783)

---
+++ hgext/review.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+
+import itertools
+import sys
+enc = sys.stdout.encoding or 'ascii'
+
+from logilab.devtools.jplproxy import build_proxy, RequestError
+from cwclientlib import builders
+
+def ask_review(client, ui, revs):
+    # def build_trinfo(eid, transition_name,  comment=None):
+    eids = client.rql(
+        '''Any P WHERE P patch_revision R, R changeset IN ({revs}),
+                       P in_state S, S name 'in-progress'
+        '''.format(revs=','.join('%r'%rev for rev in revs)))
+    queries = [builders.build_trinfo(eid[0], 'ask review') for eid in eids]
+    return client.rqlio(queries)
+
