e3637d7397d8eb6c1aedc819ab4947995aaabee7
David Douard <david.douard@logilab.fr>
2014-11-07 23:12:52 +0100

    [hgext/jpl] add a show-review hg command

--- jpl/review.py
+++ jpl/review.py
@@ -9,11 +9,16 @@ from logilab.devtools.jpl.jplproxy import build_proxy, RequestError
 from cwclientlib import builders
 
 def ask_review(client, revs):
-    # def build_trinfo(eid, transition_name,  comment=None):
     eids = client.rql(
         '''Any P WHERE P patch_revision R, R changeset IN ({revs}),
                        P in_state S, S name 'in-progress'
         '''.format(revs=','.join('%r'%rev for rev in revs)))
     queries = [builders.build_trinfo(eid[0], 'ask review') for eid in eids]
     return client.rqlio(queries)
+
+def show_review(client, revs):
+    return client.rql(
+        '''Any PN, URI, N WHERE P patch_revision R, R changeset IN ({revs}),
+             P in_state S, S name N, P cwuri URI, P patch_name PN
+        '''.format(revs=','.join('%r' % rev for rev in revs)), vid='jsonexport')
 
