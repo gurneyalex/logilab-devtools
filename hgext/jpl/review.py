3867cafee22ff0622235ad7491f2f10ded2803bc
Julien Cristau <julien.cristau@logilab.fr>
2015-02-03 15:28:07 +0100

    [hgext] add an option to choose the target version in make-ticket

--- hgext/jpl/review.py
+++ hgext/jpl/review.py
@@ -22,8 +22,13 @@ def show_review(client, revs):
              P in_state S, S name N, P cwuri URI, P patch_name PN, P patch_reviewer U?, U login L
         '''.format(revs=','.join('%r' % rev for rev in revs)), {}),])[0]
 
-def sudo_make_me_a_ticket(client, ctx):
-    query = '''INSERT Ticket T: T concerns PROJ, T title %(title)s, T description %(desc)s WHERE REV from_repository REPO, PROJ source_repository REPO, REV changeset %(cs)s'''
+def sudo_make_me_a_ticket(client, ctx, version):
+    query = '''INSERT Ticket T: T concerns PROJ, T title %%(title)s, T description %%(desc)s%s
+               WHERE REV from_repository REPO, PROJ source_repository REPO, REV changeset %%(cs)s%s'''
+    if version:
+        query %= (', T done_in V', ', V num %(version)s, V version_of PROJ')
+    else:
+        query %= ('', '')
     desc = ctx.description()
     if not desc:
         raise Exception('changeset has no description')
@@ -31,5 +36,6 @@ def sudo_make_me_a_ticket(client, ctx):
         'title': desc.splitlines()[0],
         'desc': desc,
         'cs': str(ctx),
+        'version': version,
     }
     return client.rqlio([(query, args)])
