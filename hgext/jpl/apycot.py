ad3fe2a0d4454b1d7d2b9b881d828b9cdf1070a5
David Douard <david.douard@logilab.fr>
2015-06-15 15:52:47 +0200

    [hgext] add a list-tc command to list available TestConfigs (related to #290804)

--- hgext/jpl/apycot.py
+++ hgext/jpl/apycot.py
@@ -27,3 +27,24 @@ def create_test_execution(client, changesets, label, **kwargs):
     queries = [(rql, dict(changeset=cs, **args))
                for cs in changesets]
     return client.rqlio(queries)
+
+LIST_TC_RQL = """\
+Any TCN, TCL
+WHERE
+  TC use_environment PE,
+  PE local_repository REPO,
+  CS from_repository REPO,
+  CS changeset %(changeset)s ,
+  TC label TCL, TC name TCN
+"""
+
+def list_tc(client, changesets):
+    rql = LIST_TC_RQL
+    queries = [(rql, dict(changeset=cs))
+               for cs in changesets]
+    rsets = client.rqlio(queries)
+    result = set()
+    for rset in rsets:
+        for e in rset:
+            result.add(tuple(e))
+    return result
